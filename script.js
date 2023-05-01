7420e432fab52a8c4262ad039b35c7005d05cfdf
Wesley Santos <wesley.santos@grancursosonline.com.br>
2023-04-30 23:17:51 -0300

    fixed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ inputQuestion.addEventListener("keypress", (e) => {
 
 });
 
-const OPEN_API_KEY = "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";
+const OPEN_API_KEY = "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";
 
 function sendQuestion(){
     var sQuestion = inputQuestion.value;
